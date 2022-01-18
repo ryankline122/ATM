af253e3c87974f6998b875da2eb331c396111148
Ryan Kline <ryankline4780@gmail.com>
2022-01-17 20:24:12 -0500

    Skeleton code for GUI

---
+++ ATM_GUI.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import  filedialog, Text
+import os
+
+root = tk.Tk()
+
+#Functions
+def login():
+    return 0
+
+
+def createAccount():
+    return 0
+
+canvas = tk.Canvas(root, height=700, width=700, bg='#e6e0d1')
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+# Buttons
+loginBtn = tk.Button(root, text="Login", padx=10, pady=5, fg="white", bg="blue")
+loginBtn.pack()
+
+createAccBtn = tk.Button(root, text="Create Account", padx=10, pady=5, fg="white", bg="red")
+createAccBtn.pack()
+
+root.mainloop()
+
+
+
+
+
