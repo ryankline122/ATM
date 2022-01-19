f23e53b5908d465cd6bdc0985a4d0b8dd34b9827
Ryan Kline <ryankline4780@gmail.com>
2022-01-19 18:10:55 -0500

    Renamed some functions, added import statements

---
+++ ATM.py
@@ -0,0 +1,28 @@
+class ATM:
+    def transfer():
+        return 0
+
+    def withdraw():
+        return 0
+
+    def deposit():
+        return 0
+
+    def balance():
+        return 0
+
+    def login():
+        return 0
+
+    def createAccount():
+        return 0
+
+    def closeAccount():
+        return 0
+
+    def passwordChange():
+        return 0
+
+    def usernameChange():
+        return 0
+
