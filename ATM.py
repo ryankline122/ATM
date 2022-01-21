0239a2f9e7f0eef078940b51a9f1df582ad5b23a
Ryan Kline <ryankline4780@gmail.com>
2022-01-20 21:21:20 -0500

    Created User class with some basic methods (deposit, withdraw, and changePassword)

--- ATM.py
+++ ATM.py
@@ -1,28 +1,32 @@
 from User import User
 
 class ATM:
-    def transfer(self):
-        return 0
+    # def transfer(self):
+    #     return 0
+    #
+    # def withdraw(self):
+    #     return 0
+    #
+    # def deposit(self):
+    #     return 0
+    #
+    # def balance(self):
+    #     return 0
+    #
+    # def login(self):
+    #     return 0
+    #
+    # def createAccount(self):
+    #     return 0
+    #
+    # def closeAccount(self):
+    #     return 0
+    #
+    # def passwordChange(self):
+    #     return 0
+    #
+    # def usernameChange(self):
+    #     return 0
 
-    def withdraw(self):
-        return 0
-
-    def deposit(self):
-        return 0
-
-    def balance(self):
-        return 0
-
-    def login(self):
-        return 0
-
-    def closeAccount(self):
-        return 0
-
-    def passwordChange(self):
-        return 0
-
-    def usernameChange(self):
-        return 0
-
-
+    print("Welcome to our ATM!")
+    usr = User()
