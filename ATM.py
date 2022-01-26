371316e66c20f92f294d4b931037c7c52c4d79ec
Ryan Kline <ryankline4780@gmail.com>
2022-01-26 17:37:36 -0500

    Added functionality to createAccount() and login(). Also created a temp text file for storing user data

--- ATM.py
+++ ATM.py
@@ -1,32 +1,66 @@
 from User import User
+import sys
 
-class ATM:
-    # def transfer(self):
-    #     return 0
-    #
-    # def withdraw(self):
-    #     return 0
-    #
-    # def deposit(self):
-    #     return 0
-    #
-    # def balance(self):
-    #     return 0
-    #
-    # def login(self):
-    #     return 0
-    #
-    # def createAccount(self):
-    #     return 0
-    #
-    # def closeAccount(self):
-    #     return 0
-    #
-    # def passwordChange(self):
-    #     return 0
-    #
-    # def usernameChange(self):
-    #     return 0
+def createAccount():
+    # Writes user account info to text file
+    name = input("Enter your name: ")
+    userID = input("Create a unique userID: ")
+    password = input("Create a secure password: ")
+    balance = float(input("Set initial deposit: $"))
+    file = open("user_info", "a")
+    file.write("\n" + name + "," + userID + "," + password + "," + str(balance))
 
-    print("Welcome to our ATM!")
-    usr = User()
+
+def login():
+    # Reads in user_info text file. Logs user in if userID and password inputs match
+    success = False
+    file = open("user_info", "r")
+    nameInput = input("Enter your username: ")
+    passInput = input("Enter your password: ")
+    # Loops through data for a given user in the text file. Separates items with comma
+    for i in file:
+        name, userID, password, balance = i.split(",")
+        try:
+            if (nameInput == userID and passInput == password):
+                success = True
+                break
+        except AttributeError:
+            print("No matches for " + nameInput + " enter 'new' to create new account")
+    file.close()
+
+    if (success):
+        print("Login Successful")
+    else:
+        print("Incorrect UsrID or password")
+
+
+# Runs command-line application. Provides user with commands to navigate the application
+def main():
+    commands = ["login", "new", "help", "show-balance", "deposit", "withdraw", "transfer", "exit"]
+    action = ""
+    while(action != "exit"):
+        action = input("Enter a command (Type 'help' for a list of commands): ")
+
+        if (action.lower() == "new"):
+            createAccount()
+
+        elif (action.lower() == "login"):
+            login()
+
+        elif (action.lower() == "show-balance"):
+            showBalance()
+
+        elif(action.lower() == "help"):
+            for i in commands:
+                print(i)
+
+        elif(action.lower() == "exit"):
+            sys.exit()
+        else:
+            print("Invalid command")
+
+# ATM Boot-up
+print("Welcome to our ATM!")
+main()
+
+
