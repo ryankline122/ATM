18203b763af7d7261095a11e4195e042ac710899
Lren1300 <reneaul@mail.gvsu.edu>
2022-01-26 11:25:49 -0500

    This class is where all database coding will take place

---
+++ Database.py
@@ -0,0 +1,20 @@
+import mysql.connector as mysql
+
+db = mysql.connect(
+    host = "localhost",
+    user = "LoganR",
+    passwd = "Lreneau1300798168",
+    database = "userinformation"
+)
+
+cursor = db.cursor()
+
+
+
+
+
+
+
+
+
+
