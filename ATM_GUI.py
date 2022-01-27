f90ec0c3186fa37d287d4ac55647c96af4dafa64
Selmir Lelak <lelaks@mail.gvsu.edu>
2022-01-27 01:21:17 -0500

    basic gui with pretty buttons :)

--- ATM_GUI.py
+++ ATM_GUI.py
@@ -1,27 +1,64 @@
 import tkinter as tk
-from tkinter import  filedialog, Text
+from tkinter import *
+from PIL import ImageTk, Image
 import os
-from main import ATM
-
 
 root = tk.Tk()
+root.geometry("800x400")
 
-#Functions
 
 
-canvas = tk.Canvas(root, height=400, width=800, bg='#e6e0d1')
-canvas.pack()
+
+# Functions
+
+
+#create a new user account
+def createAccount():
+        myLabel3 = Label(root, text="Testing...")
+        myLabel3.place(x=100, y=150)
+
+
+#check if valid username and password
+def checkCredentials():
+
+    myLabel = Label(root, text=User.get())
+    myLabel.place(x=400, y=355)
+
+    myLabel2 = Label(root, text=Password.get())
+    myLabel2.place(x=400, y=375)
+
+    # if valid call dashboard function
+    dashboard()
+
+# call new frame to display account information
+def dashboard():
+    dashboardFrame = Frame(root)
+
+
+
+User = Entry(root, width=30, fg='black', borderwidth=2)
+User.place(x=300, y=175)
+
+Password = Entry(root, width=30, fg='black', borderwidth=2)
+Password.place(x=300, y=205)
+
+newUserLabel = Label(root, text ="New User?", fg='black')
+newUserLabel.place(x=665, y=360)
 
 # Buttons
-loginBtn = tk.Button(root, text="Login", padx=10, pady=5, fg="white", bg="blue")
-loginBtn.pack()
+login_btn = ImageTk.PhotoImage(Image.open("login-button-png-18030_4_75x50.png"))
 
-createAccBtn = tk.Button(root, text="Create Account", padx=10, pady=5, fg="white", bg="red")
-createAccBtn.pack()
+img_label = Label(image=login_btn)
+
+
+register_btn = ImageTk.PhotoImage(Image.open("register-button-png-18477_4_50x40.png"))
+
+img_label2 = Label(image=register_btn)
+
+loginBtn = tk.Button(root, image=login_btn, command=checkCredentials, borderwidth=0)
+loginBtn.place(x=365, y=235)
+
+createAccBtn = tk.Button(root, image=register_btn, command=createAccount, borderwidth=0)
+createAccBtn.place(x=725, y=350)
 
 root.mainloop()
-
-
-
-
-
