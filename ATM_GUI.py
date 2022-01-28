1fcb8988320af135b94186705d85df5965085c5e
Selmir Lelak <lelaks@mail.gvsu.edu>
2022-01-27 19:05:48 -0500

    Made some slight changes with second frame

--- ATM_GUI.py
+++ ATM_GUI.py
@@ -18,8 +18,8 @@ def createAccount():
         myLabel3.place(x=100, y=150)
 
 
-#check if valid username and password
-def checkCredentials():
+#check if valid username and password and change frame to dashboard
+def dashboard():
 
     myLabel = Label(root, text=User.get())
     myLabel.place(x=400, y=355)
@@ -27,13 +27,44 @@ def checkCredentials():
     myLabel2 = Label(root, text=Password.get())
     myLabel2.place(x=400, y=375)
 
-    # if valid call dashboard function
-    dashboard()
+    top_Frame = LabelFrame(root, width=800, height=400,)
+    top_Frame.pack(fill="both", expand=1)
 
-# call new frame to display account information
-def dashboard():
-    dashboardFrame = Frame(root)
+    # Define a Canvas Widget
+    canvas = Canvas(top_Frame, width=390, height=150)
+    canvas.place(x=0,y=0)
 
+    canvas2 = Canvas(top_Frame, width=390, height=150)
+    canvas2.place(x=400, y=0)
+
+    # Create a rectangle in Canvas
+    canvas.create_rectangle(4, 146, 396, 4, outline='gray', width=2, fill='#343332')
+    canvas2.create_rectangle(4, 146, 396, 4, outline='gray', width=2, fill='#343332')
+
+    savingAccountLabel = Label(top_Frame, text="Saving Account", padx=10, pady=10, bg='#343332', fg='gray')
+    savingAccountLabel.place(x=5, y=5)
+
+    checkingAccountLabel = Label(top_Frame, text="Checking Account", padx=10, pady=10, bg='#343332', fg='gray')
+    checkingAccountLabel.place(x=405, y=5)
+
+    availableBalanceLabel = Label(top_Frame, text="Available Balance", padx=10, pady=10, bg='#343332', fg='gray', font="Italics 7")
+    availableBalanceLabel.place(x=405, y=105)
+
+    availableBalanceLabel = Label(top_Frame, text="Available Balance", padx=10, pady=10, bg='#343332', fg='gray', font="Italics 7")
+    availableBalanceLabel.place(x=5, y=105)
+
+
+    checkingAccountMoneyLabel = Label(top_Frame, text="$", bg='#343332', fg='gray')
+    checkingAccountMoneyLabel.place(x=405, y=85)
+
+    savingAccountMoneyLabel = Label(top_Frame, text="$", bg='#343332', fg='gray')
+    savingAccountMoneyLabel.place(x=5, y=85)
+
+    checkingAccountBalanceLabel = Label(top_Frame, text="500.00", bg='#343332', fg='gray', font="Times 18 bold")
+    checkingAccountBalanceLabel.place(x=415, y=80)
+
+    savingAccountBalanceLabel = Label(top_Frame, text="500.00", bg='#343332', fg='gray', font="Times 18 bold")
+    savingAccountBalanceLabel.place(x=15, y=80)
 
 
 User = Entry(root, width=30, fg='black', borderwidth=2)
@@ -55,7 +86,7 @@ register_btn = ImageTk.PhotoImage(Image.open("register-button-png-18477_4_50x40.
 
 img_label2 = Label(image=register_btn)
 
-loginBtn = tk.Button(root, image=login_btn, command=checkCredentials, borderwidth=0)
+loginBtn = tk.Button(root, image=login_btn, command=dashboard, borderwidth=0)
 loginBtn.place(x=365, y=235)
 
 createAccBtn = tk.Button(root, image=register_btn, command=createAccount, borderwidth=0)
