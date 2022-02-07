34f7e18a546a9ba16641dc3eb4985ef5664ee5bf
Ryan Kline <ryankline4780@gmail.com>
2022-02-06 19:18:24 -0500

    Function changes: - createAccount() now takes parameters for user info. Removed print statements - login() now takes parameters for user info. Removed print statements
    New file:
    - mainCL.py moves the main function originally in ATM.py to its own file to act as the main program file for the command-line version

--- ATM.py
+++ ATM.py
@@ -5,68 +5,17 @@ import getpass
 import sqlite3
 import sys
 
+currUser = User(None,None,None,None,None)
+
 # User is prompted to enter their name, desired username (if not taken), set their password, and put in an initial deposit
-def createAccount():
+def createAccount(name, userID, password, balance, loginStatus):
     db = sqlite3.connect('user_info.db')
     c = db.cursor()
-    # Creates user database if needed, adds user info upon successful creation
-    c.execute("""CREATE TABLE IF NOT EXISTS users(
-                    name text,
-                    userID text,
-                    password text,
-                    balance real,
-                    loginStatus text
-                )""")
-
-    name = input("Enter your name: ")
-    userID = input("Create a unique userID: ")
     # Checks if user already exists
-    (c.execute("SELECT exists(SELECT userID FROM users where userID=?)", (userID,)))
-    [exists] = c.fetchone()
-    if(exists):
-        print("Username already exists. Try again")
-    else:
-        # "Run" --> "Edit Configuration" --> Check "Emulate terminal in output console
-        password = getpass.getpass("Create your password: ", stream=None)
-        confirmPassword = getpass.getpass("Confirm your password: ")
-
-        while(confirmPassword != password):
-            print("Passwords don't match. Try again")
-            password = input("Create a secure password: ")
-            confirmPassword = input("Confirm password: ")
-
-        balance = float(input("Set initial deposit: $"))
-        usr = User(name, userID, password, balance, "False")
-        c.execute("INSERT INTO users VALUES(?,?,?,?,?)",
-                  (usr.name, usr.userID, usr.password, usr.balance, usr.loginStatus))
-        db.commit()
-    c.close()
-    db.close()
-
-
-# createAccount() for use within the GUI script
-def createAccount_GUI(name, userID, password, balance):
-    balance = float(balance)
-    # Creates user database if needed, adds user info upon successful creation
-    db = sqlite3.connect('user_info.db')
-    c = db.cursor()
-    c.execute("""CREATE TABLE IF NOT EXISTS users(
-                   name text,
-                   userID text,
-                   password text,
-                   balance real,
-                   loginStatus text
-                )""")
-    # Checks if user already exists
-    (c.execute("SELECT exists(SELECT userID FROM users where userID=?)", (userID,)))
-    [exists] = c.fetchone()
-    if (exists):
-        print("Username already exists. Try again")
-    else:
-        usr = User(name, userID, password, balance, "False")
-        c.execute("INSERT INTO users VALUES(?,?,?,?,?)",
-                  (usr.name, usr.userID, usr.password, usr.balance, usr.loginStatus))
-        db.commit()
+    usr = User(name, userID, password, balance, "False")
+    c.execute("INSERT INTO users VALUES(?,?,?,?,?)",
+            (usr.name, usr.userID, usr.password, usr.balance, usr.loginStatus))
+    db.commit()
     c.close()
     db.close()
 
@@ -85,72 +34,23 @@ def inUse():
 
 
 # Reads in user_info database. Logs user in if userID and password inputs match
-def login():
-    success = False
+def login(userID, password):
     db = sqlite3.connect('user_info.db')
     c = db.cursor()
-    if(inUse()):
-        print("Error: In use by another user")
-    else:
-        nameInput = input("Enter your username: ")
-        # "Run" --> "Edit Configuration" --> Check "Emulate terminal in output console
-        passInput = getpass.getpass("Enter your password: ", stream=None)
-        c.execute("SELECT * FROM users where userID=? AND password=?",
-                  (nameInput, passInput))
-        row = c.fetchone()
-        if row:
-            success = True
 
-        if (success):
-            print("Login Successful")
-            c.execute(
-                "UPDATE users SET loginStatus = 'True' WHERE userID =?", (nameInput,))
-            db.commit()
+    c.execute(
+        "UPDATE users SET loginStatus = 'True' WHERE userID =?", (userID,))
+    db.commit()
+    c.execute("SELECT name FROM users where userID=?", (userID,))
+    name = ','.join(c.fetchone())
+    c.execute("SELECT balance FROM users where userID=?", (userID,))
+    balance = c.fetchone()[0]
 
-            c.execute("SELECT name FROM users where userID=?", (nameInput,))
-            name = ','.join(c.fetchone())
-
-            c.execute("SELECT balance FROM users where userID=?", (nameInput,))
-            balance = c.fetchone()[0]
-
-            global currUser
-            currUser = User(name, nameInput, passInput, balance, "True")
-        else:
-            print("Incorrect UsrID or password")
-
-    c.close()
-    db.close()
-
-
-# login() for use within the GUI script
-def login_GUI(userID_Input, password_Input):
-    success = False
-
-    db = sqlite3.connect('user_info.db')
-    c = db.cursor()
-    c.execute("SELECT * FROM users where userID=? AND password=?",
-              (userID_Input, password_Input))
-    row = c.fetchone()
-    if row:
-        success = True
-
-    if (success):
-        print("Login Successful")
-        c.execute(
-            "UPDATE users SET loginStatus = 'True' WHERE userID =?", (userID_Input,))
-        db.commit()
-
-        c.execute("SELECT name FROM users where userID=?", (userID_Input,))
-        name = ','.join(c.fetchone())
-
-        c.execute("SELECT balance FROM users where userID=?", (userID_Input,))
-        balance = c.fetchone()[0]
-
-        global currUser
-        currUser = User(name, userID_Input, password_Input, balance, "True")
-    else:
-        # Change to be a pop-up alert for GUI
-        print("Incorrect UserID or Password.")
+    currUser.name = name
+    currUser.userID = userID
+    currUser.password = password
+    currUser.balance = balance
+    currUser.loginStatus = True
 
     c.close()
     db.close()
@@ -167,7 +67,6 @@ def logout():
         c.execute("UPDATE users SET loginStatus =? WHERE userID =?",
                   (currUser.loginStatus, currUser.userID,))
         db.commit()
-        print("Logging you out")
         c.close()
         db.close()
 
@@ -197,65 +96,3 @@ def updateBalance():
 
 
 # TODO: Make a deleteAccount() function
-
-
-# Runs command-line application. Provides user with commands to navigate the application
-def main():
-    commands = ["login", "new", "help", "show-balance",
-                "deposit", "withdraw", "transfer", "data", "exit"]
-    action = ""
-    while (action != "exit"):
-        action = input(
-            "Enter a command (Type 'help' for a list of commands): ")
-
-        if (action.lower() == "new"):
-            createAccount()
-
-        elif (action.lower() == "login"):
-            login()
-
-        elif (action.lower() == "show-balance"):
-            print("Balance: " + ("${:,.2f}".format(currUser.balance)))
-
-        elif (action.lower() == "deposit"):
-            amount = float(input("How much would you like to deposit? $"))
-            currUser.deposit(amount)
-            print("New Balance: " + ("${:,.2f}".format(currUser.balance)))
-            updateBalance()
-
-        # TODO: Don't let user take out more than is present in their account
-        elif (action.lower() == "withdraw"):
-            amount = float(input("How much would you like to withdraw? $"))
-            currUser.withdraw(amount)
-            print("New Balance: " + ("${:,.2f}".format(currUser.balance)))
-            updateBalance()
-
-        # TODO: Don't allow user to use transfer() on themselves
-        elif (action.lower() == "transfer"):
-            amount = float(input("How much would you like to transfer? $"))
-            recipient = input("Enter the recipients' userID: ")
-            currUser.transfer(amount, recipient)
-            print("New Balance: " + ("${:,.2f}".format(currUser.balance)))
-            updateBalance()
-
-        elif (action.lower() == "data"):
-            printData()
-
-        elif (action.lower() == "help"):
-            for i in commands:
-                print(i)
-
-        elif (action.lower() == "logout"):
-            logout()
-
-        elif (action.lower() == "exit"):
-            if(inUse()):
-                logout()
-                sys.exit()
-        else:
-            print("Invalid command")
-
-
-# ATM Boot-up
-print("Welcome to our ATM!")
-main()
---
+++ mainCL.py
@@ -0,0 +1,136 @@
+import ATM
+import getpass
+import sqlite3
+import sys
+
+# Runs command-line application. Provides user with commands to navigate the application
+def main():
+    commands = ["login", "new", "help", "show-balance",
+                "deposit", "withdraw", "transfer", "data", "exit"]
+    action = ""
+    db = sqlite3.connect('user_info.db')
+    c = db.cursor()
+    # Creates user database if needed, adds user info upon successful creation
+    c.execute("""CREATE TABLE IF NOT EXISTS users(
+                    name text,
+                    userID text,
+                    password text,
+                    balance real,
+                    loginStatus text
+                )""")
+
+    # RUNS PROGRAM UNTIL USER ENTERS "exit" COMMAND
+    while (action != "exit"):
+        action = input(
+            "Enter a command (Type 'help' for a list of commands): ")
+
+
+        # CREATE NEW ACCOUNT
+        if (action.lower() == "new"):
+            name = input("Enter your name: ")
+            userID = input("Create a unique userID: ")
+            (c.execute("SELECT exists(SELECT userID FROM users where userID=?)", (userID,)))
+            [exists] = c.fetchone()
+
+            if (exists):
+                print("Username already exists. Try again")
+            else:
+                # "Run" --> "Edit Configuration" --> Check "Emulate terminal in output console
+                pswrd = getpass.getpass("Create your password: ", stream=None)
+                confirmPassword = getpass.getpass("Confirm your password: ")
+
+                while (confirmPassword != pswrd):
+                    print("Passwords don't match. Try again")
+                    pswrd = input("Create a secure password: ")
+                    confirmPassword = input("Confirm password: ")
+
+                bal = float(input("Set initial deposit: $"))
+                ATM.createAccount(name,userID,pswrd,bal,False)
+
+
+        # LOGIN
+        elif (action.lower() == "login"):
+            success = False
+
+            if (ATM.inUse()):
+                print("Error: In use by another user")
+            else:
+                nameInput = input("Enter your username: ")
+                # "Run" --> "Edit Configuration" --> Check "Emulate terminal in output console
+                passInput = getpass.getpass("Enter your password: ", stream=None)
+                c.execute("SELECT * FROM users where userID=? AND password=?",
+                          (nameInput, passInput))
+                row = c.fetchone()
+                if row:
+                    success = True
+
+                if (success):
+                    ATM.login(nameInput, passInput)
+                    print("Login Successful")
+                else:
+                    print("Incorrect username or password")
+
+        # SHOW-BALANCE
+        elif (action.lower() == "show-balance"):
+            # TODO: Catch error if no user is logged in
+            print("Balance: " + ("${:,.2f}".format(ATM.currUser.balance)))
+
+
+        # DEPOSIT
+        elif (action.lower() == "deposit"):
+            # TODO: Catch error if no user is logged in
+            amount = float(input("How much would you like to deposit? $"))
+            ATM.currUser.deposit(amount)
+            print("New Balance: " + ("${:,.2f}".format(ATM.currUser.balance)))
+            ATM.updateBalance()
+
+
+        # WITHDRAW
+        # TODO: Don't let user take out more than is present in their account
+        elif (action.lower() == "withdraw"):
+            #TODO: Catch error if no user is logged in
+            amount = float(input("How much would you like to withdraw? $"))
+            ATM.currUser.withdraw(amount)
+            print("New Balance: " + ("${:,.2f}".format(ATM.currUser.balance)))
+            ATM.updateBalance()
+
+
+        # TRANSFER
+        # TODO: Don't allow user to use transfer() on themselves
+        elif (action.lower() == "transfer"):
+            # TODO: Catch error if no user is logged in
+            amount = float(input("How much would you like to transfer? $"))
+            recipient = input("Enter the recipients' userID: ")
+            ATM.currUser.transfer(amount, recipient)
+            print("New Balance: " + ("${:,.2f}".format(ATM.currUser.balance)))
+            ATM.updateBalance()
+
+        # PRINT DATA
+        elif (action.lower() == "data"):
+            ATM.printData()
+
+
+        # HELP
+        elif (action.lower() == "help"):
+            for i in commands:
+                print(i)
+
+
+        # LOGOUT
+        elif (action.lower() == "logout"):
+            ATM.logout()
+            print("Goodbye" + ATM.currUser.name)
+
+
+        # EXIT
+        elif (action.lower() == "exit"):
+            if(ATM.inUse()):
+                ATM.logout()
+                print("Goodbye")
+                sys.exit()
+        else:
+            print("Invalid command")
+
+# ATM Boot-up
+print("Welcome to our ATM!")
+main()
