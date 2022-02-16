d8179c829af1af84d4454f108a09c976a5789d5e
Lren1300 <reneaul@mail.gvsu.edu>
2022-02-15 22:35:04 -0500

    Pre-Test Case

--- ATM_GUI.py
+++ ATM_GUI.py
@@ -10,8 +10,6 @@ import os
 root = tk.Tk()
 root.geometry("800x400")
 
-db = sqlite3.connect('user_info.db')
-c = db.cursor()
 
 def raise_frame(frame):
     frame.tkraise()
@@ -43,10 +41,11 @@ def getCreationData():
 
     nameEntry = firstName.get()
     userNameEntry = userName1.get()
+    pinNum = secPIN1.get()
     passEntry = password1.get()
     depositEntry = deposit.get()
 
-    ATM.createAccount(nameEntry, userNameEntry, passEntry, depositEntry, False)
+    ATM.createAccount(nameEntry, userNameEntry, passEntry, depositEntry, pinNum, False)
     raise_frame(homePage)
 
 
@@ -74,11 +73,11 @@ passwordLabel.place(x=20, y=160)
 password1 = Entry(top_Frame, width=25, fg='black', borderwidth=2)
 password1.place(x=250, y=170)
 
-securityQuestionLabel = Label(top_Frame, text="Security Question: What is your favorite color?",
+securityPINNLabel = Label(top_Frame, text="Add a PIN number of 4 digits.",
                                   padx=15, pady=15, bg='#343332', fg='white', font="Italics 7")
-securityQuestionLabel.place(x=20, y=230)
-secQuestion = Entry(top_Frame, width=25, fg='black', borderwidth=2)
-secQuestion.place(x=275, y=245)
+securityPINNLabel.place(x=20, y=230)
+secPIN1 = Entry(top_Frame, width=25, fg='black', borderwidth=2)
+secPIN1.place(x=275, y=245)
 
 initialDepositLabel = Label(top_Frame, text="What would you like your initial deposit to be?",
                                   padx=15, pady=15, bg='#343332', fg='white', font="Italics 7")
@@ -90,18 +89,19 @@ updateButton = tk.Button(top_Frame, text="Add to database!", padx=17, pady=17, f
                          , command=lambda:getCreationData())
 updateButton.place(x=600, y=225)
 
-#doneButton = tk.Button(top_Frame, text="All Done!", padx=17, pady=17, fg="white", bg='#343332',
-                       #command=lambda:raise_frame(Top_Frame))
-#doneButton.place(x=600, y=300)
-
-
 
 #passChange Frame Logan Reneau
 def passChangeData():
-   userNameData = userName2.get()
-   newPass = password2.get()
-   ATM.updatePassword(newPass, userNameData)
-   raise_frame(homePage)
+    userNameData = userName2.get()
+    pinNumIn = secPIN.get()
+    newPass = password2.get()
+
+    if ATM.updatePassword(newPass, userNameData, pinNumIn) == False:
+        errorLabelPass = Label(top_Frame2, text="Error: Incorrect userID or PIN", fg='Black',
+                               font='Italics 12')
+        errorLabelPass.place(x=575, y=200)
+    else:
+        raise_frame(homePage)
 
 
 
@@ -120,11 +120,11 @@ userNameLabel.place(x=250, y=135)
 userName2 = Entry(top_Frame2, width=25, fg='black', borderwidth=2)
 userName2.place(x=400, y=150)
 
-securityQuestionLabel = Label(top_Frame2, text="What is your favorite color?",
+securityPINLabel = Label(top_Frame2, text="What is your PIN number?",
                                   padx=15, pady=15, bg='#343332', fg='white', font="Italics 7")
-securityQuestionLabel.place(x=235, y=210)
-secQuestion = Entry(top_Frame2, width=25, fg='black', borderwidth=2)
-secQuestion.place(x=400, y=225)
+securityPINLabel.place(x=235, y=210)
+secPIN = Entry(top_Frame2, width=25, fg='black', borderwidth=2)
+secPIN.place(x=400, y=225)
 
 passwordLabel = Label(top_Frame2, text="What would you like your  new password to be?",
                           padx=15, pady=15, bg='#343332', fg='white', font="Italics 7")
@@ -137,42 +137,9 @@ doneButton = tk.Button(top_Frame2, text="All Done!", padx=17, pady=17, fg="white
 doneButton.place(x=600, y=300)
 
 
-#moneyMoves Frame Logan Reneau
-top_Frame3 = LabelFrame(moneyMoves, width=800, height=400)
-top_Frame3.pack(fill="both", expand=1)
-
-canvasM = Canvas(top_Frame3, width=800, height=400, bg='#75706F')
-canvasM.place(x=0, y=0)
-
-moneyTransfer = Label(top_Frame3, text="Would you like to deposit or withdraw?",
-                          padx=10, pady=10, fg="black", font="Italics 15", bg='#75706F')
-moneyTransfer.place(x=225, y=20)
-
-moneyInputLabel = Label(top_Frame3, text="How much?",
-                          padx=10, pady=10, fg="black", font="Italics 15", bg='#75706F')
-moneyInputLabel.place(x=325, y=120)
-
-moneyInput = Entry(top_Frame3, width=23, fg='black', borderwidth=2)
-moneyInput.place(x=320, y=175)
-#moneyInput.get() gets the value within.
-
-doneButton2 = tk.Button(top_Frame3, text="Submit", padx=17, pady=17, fg="white", bg='#343332',
-                        command=lambda:raise_frame(homePage))
-doneButton2.place(x=600, y=300)
-
-options = [
-    "Deposit",
-    "Withdraw"
-]
-
-myCombo = ttk.Combobox(top_Frame3, value=options)
-myCombo.current(0)
-myCombo.pack(pady=80)
-
 # check if valid username and password and change frame to dashboard
 #Selmir
 def dashboard():
-
     success = False
     Username = User.get()
     Passw = Password.get()
@@ -187,9 +154,8 @@ def dashboard():
         success = True
 
     if (success):
-      ATM.login(Username, Passw)
-      Top_Frame()
-      ATM.logout()
+        ATM.login(Username, Passw)
+        raise_frame(Top_Frame)
 
     else:
         myLabel4 = Label(root, text="Incorrect username or password", fg='red', font="Times 12 bold")
@@ -201,19 +167,18 @@ top_FrameOG = LabelFrame(Top_Frame, width=800, height=400)
 top_FrameOG.pack(fill="both", expand=1)
 
 # Define a Canvas Widget
-canvasT = Canvas(top_FrameOG, width=390, height=150)
-canvasT.place(x=0, y=0)
 
-canvasT2 = Canvas(top_FrameOG, width=390, height=150)
-canvasT2.place(x=400, y=0)
+canvasT4 = Canvas(top_FrameOG, width=800, height=400, bg='#75706F')
+canvasT4.place(x=0, y=0)
 
-canvasT3 = Canvas(top_FrameOG, width=700, height=150)
+canvasT = Canvas(top_FrameOG, width=390, height=150, bg='#343332')
+canvasT.place(x=5, y=5)
+
+canvasT2 = Canvas(top_FrameOG, width=390, height=150, bg='#343332')
+canvasT2.place(x=395, y=5)
+
+canvasT3 = Canvas(top_FrameOG, width=700, height=150, bg='#343332')
 canvasT3.place(x=50, y=180)
-
-    # Create a rectangle in Canvas
-canvasT.create_rectangle(4, 146, 396, 4, outline='gray', width=2, fill='#343332')
-canvasT2.create_rectangle(4, 146, 396, 4, outline='gray', width=2, fill='#343332')
-canvasT3.create_rectangle(4, 146, 696, 4, outline='gray', width=2, fill='#343332')
 
 recentLogLabel = Label(top_FrameOG, text="Recent Transactions", padx=10, pady=10, bg='#343332', fg='gray',
                            font='Times 10 bold')
@@ -245,10 +210,69 @@ checkingAccountBalanceLabel.place(x=415, y=80)
 savingAccountBalanceLabel = Label(top_FrameOG, text="500.00", bg='#343332', fg='gray', font="Times 18 bold")
 savingAccountBalanceLabel.place(x=15, y=80)
 
+logoutButton = tk.Button(top_FrameOG, text="Logout", command=lambda:logout())
+logoutButton.place(x=200, y=270)
+
+moneyMovesButton = tk.Button(top_FrameOG, text="Deposit/Withdraw Screen", padx=5, pady=5, fg="white", bg='#343332', command=lambda:raise_frame(moneyMoves))
+moneyMovesButton.place(x=570,y=270)
+
+
+def moneymoves():
+    if ATM.currUser.loginStatus:
+        money = moneyInput.get()
+        if myCombo.get() == "Deposit":
+            ATM.updateBalance(True, money)
+            raise_frame(top_FrameOG)
+        else:
+            ATM.updateBalance(False, money)
+            raise_frame(top_FrameOG)
+
+
+def logout():
+    ATM.logoutAll()
+    raise_frame(homePage)
+
+
+#moneyMoves Original Logan Reneau
+top_Frame3 = LabelFrame(moneyMoves, width=800, height=400)
+top_Frame3.pack(fill="both", expand=1)
+
+canvasM = Canvas(top_Frame3, width=800, height=400, bg='#75706F')
+canvasM.place(x=0, y=0)
+
+moneyTransfer = Label(top_Frame3, text="Would you like to deposit or withdraw?",
+                          padx=10, pady=10, fg="black", font="Italics 15", bg='#75706F')
+moneyTransfer.place(x=225, y=20)
+
+moneyInputLabel = Label(top_Frame3, text="How much?",
+                          padx=10, pady=10, fg="black", font="Italics 15", bg='#75706F')
+moneyInputLabel.place(x=325, y=120)
+
+moneyInput = Entry(top_Frame3, width=23, fg='black', borderwidth=2)
+moneyInput.place(x=320, y=175)
+#moneyInput.get() gets the value within.
+
+doneButton2 = tk.Button(top_Frame3, text="Submit", padx=17, pady=17, fg="white", bg='#343332',
+                        command=lambda:moneymoves())
+doneButton2.place(x=600, y=300)
+
+options = [
+    "Deposit",
+    "Withdraw"
+]
+
+myCombo = ttk.Combobox(top_Frame3, value=options)
+myCombo.current(0)
+myCombo.pack(pady=80)
+
+
 #update balances,etc...
 #Selmir
+
+
 def update_topFrame():
     Tk.update()
+
 
 #homePage Frame Original Ryan K, Updated by Logan Reneau
 User = Entry(homePage, width=30, fg='black', borderwidth=2)
@@ -271,7 +295,8 @@ register_btn = ImageTk.PhotoImage(Image.open("register-button-png-18477_4_50x40.
 
 img_label2 = Label(image=register_btn)
 
-loginBtn = tk.Button(homePage, image=login_btn, command=dashboard, borderwidth=0)
+loginBtn = tk.Button(homePage, image=login_btn, command=lambda:dashboard(),
+                    borderwidth=0)
 loginBtn.place(x=355, y=235)
 
 createAccBtn = tk.Button(homePage, image=register_btn, command=lambda:raise_frame(createAccount), borderwidth=0)
@@ -279,10 +304,6 @@ createAccBtn.place(x=725, y=350)
 
 forgotPasswordButton = tk.Button(homePage, text="Forgot Password?", padx=5, pady=5, fg="white", bg='#343332', command=lambda:raise_frame(passwordChange))
 forgotPasswordButton.place(x=670,y=310)
-
-forgotPasswordButto = tk.Button(homePage, text="Deposit/Withdraw Screen", padx=5, pady=5, fg="white", bg='#343332', command=lambda:raise_frame(moneyMoves))
-forgotPasswordButto.place(x=630,y=270)
-
 
 
 raise_frame(homePage)
