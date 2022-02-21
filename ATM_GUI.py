4f21447c1d63dc25fd80e3c307adfa7643ca8004
Ryan Kline <ryankline4780@gmail.com>
2022-02-20 19:36:11 -0500

    fixed inital deposit

--- ATM_GUI.py
+++ ATM_GUI.py
@@ -416,8 +416,8 @@ def checkEntryBoxes():
         messagebox.showerror("Invalid input", "You cannot leave \"Confirm PIN\" empty")
     else:
         try:
-            int(deposit.get())
-            if(int(deposit.get()) <0 or int(deposit.get()) > ATM.MAX_BALANCE):
+            float(deposit.get())
+            if(float(deposit.get()) < 0 or float(deposit.get()) > ATM.MAX_BALANCE):
                 messagebox.showerror("Invalid input for initial deposit",
                                  "Your deposit must be an integer that is >0 and <999999999999")
             else:
